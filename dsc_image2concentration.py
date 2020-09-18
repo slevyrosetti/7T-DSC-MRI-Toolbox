267e01a2214ca0fd05a4e44d81fd28520e968e95
Simon Levy-Rosetti <simon.levy@mines-ales.org>
2020-09-18 18:04:40 +0200

    OPT: enable to provide physiolog file name, injection repetition number, TE and bolus timings instead of extracting those parameters automatically from the processing .sh file or to provide the path to the processing .sh file directly

--- dsc_image2concentration.py
+++ dsc_image2concentration.py
@@ -17,7 +17,7 @@ import argparse
 import sys
 import os
 
-def main(iFname, maskFname, paramFilePath, physioLogFolder, oFname, r2GdInBlood):
+def main(iFname, maskFname, paramFilePath, oFname, r2GdInBlood, physioLogFname, TE, injRep, firstPassStart, firstPassEnd):
     """Main.
     All times are in milliseconds from now on.
     """
@@ -33,9 +33,10 @@ def main(iFname, maskFname, paramFilePath, physioLogFolder, oFname, r2GdInBlood)
     # ----------------------------------------------------------------------------------------------------------------------
     # Physio processing
     # ----------------------------------------------------------------------------------------------------------------------
-    subjID = os.path.abspath(iFname).split('/')[-4]
-    baseDir = '/'.join(os.path.abspath(iFname).split('/')[0:-4])
-    physioLogFname, TE, injRep, gap, TR, acqTime_firstImg, firstPassStart, firstPassEnd, resolution = dsc_utils.get_physiologFname_TE_injRep(subjID, filename=paramFilePath, baseDir=baseDir)
+    if not (physioLogFname and TE and injRep):
+        subjID = os.path.abspath(iFname).split('/')[-3]
+        baseDir = '/'.join(os.path.abspath(iFname).split('/')[0:-3])
+        physioLogFname, TE, injRep, gap, TR, acqTime_firstImg, firstPassStart, firstPassEnd, resolution = dsc_utils.get_physiologFname_TE_injRep(subjID, filename=paramFilePath, baseDir=baseDir)
     repsAcqTime, timePhysio, valuesPhysio = dsc_utils.extract_acqtime_and_physio_by_slice(physioLogFname, img.shape[2], img.shape[3], acqTime_firstImg, TR=TR)
     # repsAcqTime_PulseOx, Time_PulseOx, values_PulseOx, repsAcqTime_Resp, Time_Resp, values_Resp = dsc_utils.extract_acqtime_and_physio(physioLogFname, img.shape[3])
     # reorder the acquisition times of each slice according to the acquisition scheme ("interleaved", "ascending",
@@ -203,17 +204,14 @@ if __name__ == "__main__":
 
     requiredArgs.add_argument('-i', dest='iFname', help='Path to MRI data file.', type=str, required=True)
     requiredArgs.add_argument('-m', dest='maskFname', help='NIFTI volume defining the region of interest.', type=str, required=True)
-    requiredArgs.add_argument('-physio', dest='physioLogFolder', help='Path to folder including the physiologs.', type=str, required=True)
     requiredArgs.add_argument('-o', dest='oFname', help='Filename for output image and mat file.', type=str, required=True)
-    # requiredArgs.add_argument('-inj', dest='injRep', help='Repetition number of contrast agent injection.', type=int, required=True)
 
     optionalArgs.add_argument('-param', dest='paramFilePath', help='Path to file giving specific parameters (injection repetition, dicom path).', type=str, required=False, default='')
-    # optionalArgs.add_argument('-s', dest='firstPassStartTime', help='Start time (on original time grid) of first pass (in seconds).', type=float, required=False, default=50.0)
-    # optionalArgs.add_argument('-e', dest='firstPassEndTime', help='Time (on original time grid) of first pass end (in seconds).', type=float, required=False, default=71.0)
-    # optionalArgs.add_argument('-b1', dest='FAmapFname', help='Path to flip angle map NIFTI.', type=str, required=False, default='')
-    # optionalArgs.add_argument('-FAparams', dest='FAparams', help='Voltage used for flip angle map, voltage used for DSC MRA data, flip angle for DSC MRI data with'
-    #                                                             ' format <FA map voltage>,<DSC voltage>,<DSC flip angle>', type=str, required=False, default='0,0,0')
-    # optionalArgs.add_argument('-te', dest='TE', help='Echo time in milliseconds.', type=float, required=False, default=43.0)
+    optionalArgs.add_argument('-physio', dest='physioLogFname', help='Basename of physio log for Pulse Ox and Respiration.', type=str, required=False, default='')
+    optionalArgs.add_argument('-inj', dest='injRep', help='Number of the repetition when contrast agent injection was launched.', type=int, required=False, default=0)
+    optionalArgs.add_argument('-s', dest='firstPassStartTime', help='Start time (on original time grid) of first pass (in seconds).', type=float, required=False, default=50.0)
+    optionalArgs.add_argument('-e', dest='firstPassEndTime', help='Time (on original time grid) of first pass end (in seconds).', type=float, required=False, default=71.0)
+    optionalArgs.add_argument('-te', dest='TE', help='Echo time in milliseconds.', type=float, required=False, default=0.0)
     optionalArgs.add_argument('-r2', dest='r2GdInBlood', help='Transverve relaxivity (in s-1.mmol-1.L = s-1.mM-1) of Gadolinium in blood.'
                                                      ' Default = 3.55 s-1.mmol-1.L [from Proc. Intl. Soc. Mag. Reson. Med. 16 (2008) 1457]', type=float, required=False, default=3.55)
     parser._action_groups.append(optionalArgs)
@@ -222,4 +220,5 @@ if __name__ == "__main__":
     # FAparamsArgs = np.array(args.FAparams.strip().split(','), dtype=float)
 
     # run main
-    main(iFname=args.iFname, maskFname=args.maskFname, paramFilePath=args.paramFilePath, physioLogFolder=args.physioLogFolder, oFname=args.oFname, r2GdInBlood=args.r2GdInBlood)
+    main(iFname=args.iFname, maskFname=args.maskFname, oFname=args.oFname, r2GdInBlood=args.r2GdInBlood, physioLogFname=args.physioLogFname, injRep=args.injRep, firstPassStart=1000*args.firstPassStartTime,
+         firstPassEnd=1000*args.firstPassEndTime, TE=args.TE, paramFilePath=args.paramFilePath)
